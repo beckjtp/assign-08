ad608008b442bfc9de79de585674316ea0302efc
Jitrin Panitdirake <jitrinpan@mathregit.group>
2020-07-21 16:38:51 +0700

    commit

--- product-app/src/app/product-list/product-list.component.ts
+++ product-app/src/app/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from './../model/product';
 
 @Component({
   selector: 'app-product-list',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-
+  products:Product[]=[];
   constructor() { }
 
   ngOnInit(): void {
+    const p1 = new Product('01','ข้าวมันไก่',60,true,5.0);
+    const p2 = new Product('02','ข้าวผัด',40,true,4.0);
+    const p3 = new Product('03','ยำวุ้นเส้น',50,false,4.5);
+    this.products.push(p1,p2,p3);
   }
 
 }
